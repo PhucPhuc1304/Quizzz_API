108b91e67d2e79a30814a4fe5d39883975517c81
Hieu Pham <minhhieupham2002@gmail.com>
2024-01-03 18:48:31 +0700

    done api questions

--- validates/question.js
+++ validates/question.js
@@ -5,10 +5,10 @@ module.exports = {
   validator: function () {
     return [
       body("questionText", "Câu hỏi không được để trống").notEmpty(),
-      body("questionA", "Câu trả lời A không được để trống").notEmpty(),
-      body("questionB", "Câu trả lời B không được để trống").notEmpty(),
-      body("questionC", "Câu trả lời C không được để trống").notEmpty(),
-      body("questionD", "Câu trả lời D không được để trống").notEmpty(),
+      body("answerA", "Câu trả lời A không được để trống").notEmpty(),
+      body("answerB", "Câu trả lời B không được để trống").notEmpty(),
+      body("answerC", "Câu trả lời C không được để trống").notEmpty(),
+      body("answerD", "Câu trả lời D không được để trống").notEmpty(),
       body("correctAnswer", "Câu trả lời đúng không được để trống").notEmpty(),
       body("subject", "Chủ đề không được để trống").notEmpty(),
       body("difficultyLevel", "Mức độ khó không được để trống").notEmpty(),
